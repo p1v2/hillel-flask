eb42085e11a768f1d34089e44f5267876de9d470
VasylSiromakha <a0661833889@gmail.com>
2023-01-31 20:44:02 +0200

    Add PUT book

--- test_server.py
+++ test_server.py
@@ -1,7 +1,14 @@
 import requests
 
-response = requests.delete(
-    "http://127.0.0.1:5000/books/1"
+# response = requests.post(
+#     "http://127.0.0.1:5000/books",
+#     json={"name": "qwerty"}
+# )
+
+
+response = requests.put(
+    "http://127.0.0.1:5000/books/2",
+    json={"name": "new_book_Author_Siromakha_Vasyl"}
 )
 
 print(response.status_code)
