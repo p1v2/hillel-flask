b82610e21386d4ae4cb6f93763dc3c71307f8669
Gregory <gregory@gmail.com>
2023-02-04 19:36:37 +0200

    git commit

--- test_server.py
+++ test_server.py
@@ -1,7 +1,8 @@
 import requests
 
 response = requests.delete(
-    "http://127.0.0.1:5000/books/1"
+    "http://127.0.0.1:5000/books/3",
+    json={"name": "LOTR"}
 )
 
 print(response.status_code)
